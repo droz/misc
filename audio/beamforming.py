1ab5ca5e55bccc138009473109bfb1fe0daf0b9b
droz <py.droz@gmail.com>
2025-05-31 15:33:32 -0700

    Refactored code to generate waveforms based on time linear interpolation

--- audio/beamforming.py
+++ audio/beamforming.py
@@ -9,21 +9,40 @@ SPEED_OF_SOUND_M_S = 343.0
 
 # Load audio data from file and write it as a numpy array
 track1_file = "/Users/droz/Documents/GitHub/misc/audio/samples/subject1.wav"
-track2_file = "/Users/droz/Documents/GitHub/misc/audio/samples/subject1.wav"
+track2_file = "/Users/droz/Documents/GitHub/misc/audio/samples/subject2.wav"
 
 track1, sr1 = sf.read(track1_file)
 track2, sr2 = sf.read(track2_file)
 # Ensure both tracks have the same sample rate
 if sr1 != sr2:
     raise ValueError("Sample rates of the two tracks do not match.")
+# Pad the shorter track with zeros to match the length of the longer track
+if len(track1) < len(track2):
+    track1 = np.pad(track1, (0, len(track2) - len(track1)), mode='constant')
+elif len(track2) < len(track1):
+    track2 = np.pad(track2, (0, len(track1) - len(track2)), mode='constant')
+# Loop the tracks a few times
+track1 = np.tile(track1, 5)
+track2 = np.tile(track2, 5)
+
+# Use the longest of the two tracks to figure out the length of the output
+tmax = len(track1) / sr1  # in seconds
+t = np.linspace(0, tmax, num=len(track1))  # time vector
+num_samples = t.shape[0]  # total number of samples in the output
 
 # The location of the two points we are trying to project to
-target1 = np.array([1, 0, 5])  # meters
-target2 = np.array([-1, 0, 5])  # meters
+target1 = np.array([0, 0, 5])  # meters
+target2 = np.array([2, 0, 5])  # meters
 
-# This is the location of each speaker in the array. They are arranged on a regular grid.
-num_speakers_x = 20
-num_speakers_y = 20
+# The position of the listener in the room
+# For now we are just moving the listener between two points in space
+point1 = np.array([-3, 0, 5])  # meters
+point2 = np.array([3, 0, 5])  # meters
+listener_pos = np.linspace(point1, point2, num=num_samples)
+
+# This is the position of each speaker in the array. They are arranged on a regular grid.
+num_speakers_x = 100
+num_speakers_y = 1
 speaker_spacing = 0.1  # meters
 speakers_x = np.linspace(-num_speakers_x / 2 * speaker_spacing, num_speakers_x / 2 * speaker_spacing, num_speakers_x)
 speakers_y = np.linspace(-num_speakers_y / 2 * speaker_spacing, num_speakers_y / 2 * speaker_spacing, num_speakers_y)
@@ -31,49 +50,38 @@ speakers_x, speakers_y = np.meshgrid(speakers_x, speakers_y)
 speakers_x = speakers_x.flatten()
 speakers_y = speakers_y.flatten()
 speakers_z = np.zeros_like(speakers_x)  # Assuming all speakers are at z=0
-speakers = np.vstack((speakers_x, speakers_y, speakers_z)).T
+speakers_pos = np.vstack((speakers_x, speakers_y, speakers_z)).T
 
-# Calculate the delay in seconds for each speaker based on the distance to the target point
-def calculate_delays(speakers, target, speed_of_sound):
-    delays = np.zeros(speakers.shape[0])
-    for i, speaker in enumerate(speakers):
-        distance = np.linalg.norm(speaker - target)
-        delays[i] = distance / speed_of_sound
+def propagation_delays(speaker_pos, targets, speed_of_sound):
+    """ Calculate the propagation delay in seconds for a speaker based on the distance to the target point
+    Args:
+        speaker_pos (np.ndarray): speaker position (3).
+        target_pos (np.ndarray): Target positions, as a function of time (3, N).
+        speed_of_sound (float): Speed of sound in m/s.
+    """
+    if targets.ndim == 1:
+        distances = np.linalg.norm(speaker_pos - targets, axis=0)
+    else:
+        distances = np.linalg.norm(speaker_pos - targets, axis=1)
+    delays = distances / speed_of_sound
     return delays
 
-delays1 = calculate_delays(speakers, target1, SPEED_OF_SOUND_M_S)
-delays2 = calculate_delays(speakers, target2, SPEED_OF_SOUND_M_S)
+# For each speaker, we compute the waveform at our current position in time
+audio = np.zeros(num_samples)
+for speaker_pos in speakers_pos:
+    beamforming_delay1 = - propagation_delays(speaker_pos, target1, SPEED_OF_SOUND_M_S)
+    beamforming_delay2 = - propagation_delays(speaker_pos, target2, SPEED_OF_SOUND_M_S)
 
-# Create a delayed version of the audio track for each speaker and sum them up
-def apply_delays(track, delays, sample_rate):
-    delayed_tracks = []
-    # First we pad all the start of all the tracks
-    for delay in delays:
-        delay_samples = int(delay * sample_rate)
-        # Create a zero-padded version of the track with the delay applied
-        delayed_track = np.zeros(len(track) + delay_samples)
-        delayed_track[delay_samples:] = track
-        delayed_tracks.append(delayed_track)
-    # Now we should pad the end of the tracks to make them all the same length
-    max_length = max(len(t) for t in delayed_tracks)
-    for i in range(len(delayed_tracks)):
-        if len(delayed_tracks[i]) < max_length:
-            delayed_tracks[i] = np.pad(delayed_tracks[i], (0, max_length - len(delayed_tracks[i])), 'constant')
-    # Finally, we sum all the delayed tracks together
-    return np.sum(delayed_tracks, axis=0) / len(delays)
+    # compute the delay for the listener position
+    listener_delays = propagation_delays(speaker_pos, listener_pos, SPEED_OF_SOUND_M_S)
+    # Sample the audio track at the appropriate time for each speaker
+    delays1 = beamforming_delay1 + listener_delays
+    delays2 = beamforming_delay2 + listener_delays
+    audio += np.interp(t, t - delays1, track1)
+    #audio += np.interp(t, t - delays2, track2, left=0, right=0)
 
-random_delays1 = np.random.uniform(0, 0.1, size=delays1.shape)  # Randomize delays for testing
-
-delayed_track1 = apply_delays(track1, delays1, sr1)
-random_track1 = apply_delays(track1, random_delays1, sr1)
-
-
-
+audio /= len(speakers_pos)  # Normalize by the number of speakers
 
 # Playback the audio tracks
-sd.play(track1, samplerate=sr1)
-sd.wait()  # Wait until the audio is finished playing
-sd.play(delayed_track1, samplerate=sr1)
-sd.wait()  # Wait until the audio is finished playing
-sd.play(random_track1, samplerate=sr1)
+sd.play(audio, samplerate=sr1)
 sd.wait()  # Wait until the audio is finished playing
